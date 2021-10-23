ca20ec8bb7cdf1440db2c3b2a76414901da7092e
urjit <urjit.desai@quantiphi.com>
2021-10-23 15:51:16 -0700

    images

---
+++ precache-manifest.82c7b6c7d98e8b1f352ae15984af6333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07359e688acb1dd582d1f8ac4dd49704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7f8a34ee32f6b6086b",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7f8a34ee32f6b6086b",
+    "url": "/static/js/main.ee45e286.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
